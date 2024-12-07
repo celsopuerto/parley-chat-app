a1445fb12f1c9d2babac9ac554d10d12674a53c1
Celso M. Puerto Jr. <isoygwapo84@gmail.com>
2024-12-07 19:58:55 +0800

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { useThemeStore } from "../src/store/useThemeStore";
+
+const theme = useThemeStore.getState().theme;
+document.documentElement.setAttribute("data-theme", theme);
+
+// Listen for theme changes and update the HTML attribute
+useThemeStore.subscribe((state) => {
+  document.documentElement.setAttribute("data-theme", state.theme);
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
